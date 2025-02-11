6d8129b659adaa16b3d2258767cfa381d35efc58
Jack Naughton <jacktnaughton@gmail.com>
2025-02-11 14:42:38 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector(".typing");
+    setTimeout(() => {
+        text.style.borderRight = "none";
+    }, 3000);
+});
+
